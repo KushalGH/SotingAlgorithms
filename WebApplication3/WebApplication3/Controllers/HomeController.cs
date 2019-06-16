85f39dce16556cdbe63ee835fd6c9c6dcccc7da0
KushalGH <kushalseth1408@gmail.com>
2019-06-17 02:27:36 +0530

    bubble and insertion sort

---
+++ WebApplication3/WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Sorts;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            BubbleSort<int> bubbleSort = new BubbleSort<int>();
+
+            Random random = new Random();
+            int[] items = new int[5];
+            List<int> vn = new List<int>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                vn.Add(random.Next());
+            }
+
+            items = vn.ToArray();
+
+            bubbleSort.Sort(items);
+
+            return View();
+        }
+
+        public ActionResult InsertionSort()
+        {
+
+            InsertionSort<int> insertionSort = new InsertionSort<int>();
+
+            Random random = new Random();
+            int[] items = new int[5];
+            List<int> vn = new List<int>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                vn.Add(random.Next());
+            }
+
+            items = vn.ToArray();
+
+            //
+            items = new int[] { 5,3,4,4,9,6,7,1,8 };
+
+            insertionSort.Sort(items);
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication3/WebApplication3/Sorts/BubbleSort.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace WebApplication3.Sorts
+{
+    public class BubbleSort<T> where T : IComparable<T>
+    {
+        public void Sort(T[] items)
+        {
+            bool swapped; int compare = 0; int swap = 0;
+            DateTime dateTime1 = DateTime.Now;
+            do
+            {
+                swapped = false;
+                for (int i = 0; i < items.Length; i++)
+                {
+                    compare = compare + 1;
+                    if ((i + 1 < items.Length) && items[i].CompareTo(items[i + 1]) > 0)
+                    {
+                        swap = swap + 1;
+                        Swap(items, i, i + 1);
+                        swapped = true;
+                    }
+                }
+            }
+            while (swapped != false);
+            DateTime dateTime2 = DateTime.Now;
+            var diffInSeconds = (dateTime2 - dateTime1).TotalSeconds;
+        }
+
+        private void Swap(T[] items, int left, int right)
+        {
+            if (left != right)
+            {
+                T temp = items[left];
+                items[left] = items[right];
+                items[right] = temp;
+            }
+        }
+    }
+}
